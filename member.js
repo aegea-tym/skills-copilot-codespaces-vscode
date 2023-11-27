fcaa3c9cae10d3f4316c022a80430c2958b962f7
Antonio Egea <aegea@tym.es>
2023-11-27 17:58:37 +0100

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var memberValue = member.options[member.selectedIndex].value;
+    var memberText = member.options[member.selectedIndex].text;
+    var memberSkills = document.getElementById("memberSkills");
+    memberSkills.innerHTML = "You have selected " + memberText + " with a value of " + memberValue;
+}
